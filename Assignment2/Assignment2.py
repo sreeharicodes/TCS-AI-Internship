3b52a34f3a2725dd5cf9d82a3745967a3620d4a5
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-02-17 23:07:45 +0530

    assignment2completed

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -8,20 +8,26 @@ df1 =pd.read_csv('ign.csv')
 df2 = pd.read_csv('ign_score.csv')
 df = pd.merge(df1,df2, on='id')
 
+df.iloc[18365,11] = '6.4'
+df=df.loc[:, ~df.columns.str.contains('^Unnamed')]
 #2. Provide the names of 10 movies rated highest.
-top = df.sort_values(by='score',ascending=False)[:10]
-print(top['title'])
+top = df.sort_values(by='score',ascending=False)
+top = top['title'].unique()[:10]
+print(top)
 
 #3. Rank the movie names by their highest average rating scores.
 #found that score is not numeric ,so while converting to float one row of score \
 # field is playstaion beta so i gave an average value 7 instead of that converted the score field to float
-print("df.iloc[18365,11] >before")
-print(df.iloc[18365,11])
-df.iloc[18365,11] = '7.0'
-print("df.iloc[18365,11] >after")
-print(df.iloc[18365,11])
+# print("df.iloc[18365,11] >before")
+# print(df.iloc[18365,11])
+# df.iloc[18365,11] = '7.0'
+# print("df.iloc[18365,11] >after")
+# print(df.iloc[18365,11])
 
 df['score']=df['score'].astype(float)
 
 rank = df.groupby('title').score.mean().sort_values(ascending=False)
-print(rank)
+r = rank.rank(method ='dense',ascending=False)
+#print(rank)
+df = df.assign(Rank=r)
+print(df)
