ed9436c524757e837902f3b7bac3bc8cbaed0cc8
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-02-17 23:20:17 +0530

    assignment2 finished

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -8,26 +8,27 @@ df1 =pd.read_csv('ign.csv')
 df2 = pd.read_csv('ign_score.csv')
 df = pd.merge(df1,df2, on='id')
 
-df.iloc[18365,11] = '6.4'
+df.iloc[18365,11] = '7.0'
 df=df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
 #2. Provide the names of 10 movies rated highest.
 top = df.sort_values(by='score',ascending=False)
-top = top['title'].unique()[:10]
-print(top)
+print(top[:10]['title'])
 
 #3. Rank the movie names by their highest average rating scores.
-#found that score is not numeric ,so while converting to float one row of score \
-# field is playstaion beta so i gave an average value 7 instead of that converted the score field to float
-# print("df.iloc[18365,11] >before")
-# print(df.iloc[18365,11])
-# df.iloc[18365,11] = '7.0'
-# print("df.iloc[18365,11] >after")
-# print(df.iloc[18365,11])
-
 df['score']=df['score'].astype(float)
-
 rank = df.groupby('title').score.mean().sort_values(ascending=False)
-r = rank.rank(method ='dense',ascending=False)
-#print(rank)
-df = df.assign(Rank=r)
-print(df)
+print(rank)
+
+#4. Plot movie scores across each genre.
+df.hist(by="genre", column="score", figsize=(25,50),grid =True)
+plt.show()
+
+#5. Find the group that provides the highest average movie ratings when split into genre groups.
+hamr = df.groupby('genre')[['score']].mean()
+hamr = hamr.sort_values(by='score',ascending=False)
+print(hamr.head(1))
+
+#6. Provide a table with the average rating of a movie by each genre group along with the movie title.
+table = pd.pivot_table(df, values='score', index=['genre', 'title'])
+print(table)
